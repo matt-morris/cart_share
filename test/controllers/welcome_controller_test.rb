7a18261c23f19bd2895decda14e7e81393c9368f
Matt Morris <double.emms@gmail.com>
2017-01-14 22:49:50 -0700

    add basic test for setting up ci/cd

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "200 ok" do
+    get '/'
+    assert response.status == 200
+  end
 end
